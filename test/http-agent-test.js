4d90daa48ab01d0614bc34ee1a606a3bfea736e7
indexzero <charlie.robbins@gmail.com>
2010-06-25 03:40:20 -0400

    Almost got request chaining working

--- test/http-agent-test.js
+++ test/http-agent-test.js
@@ -10,18 +10,28 @@ require.paths.unshift(path.join(__dirname, '..', 'lib'));
 
 var httpAgent = require('http-agent');
 
+function createAgent (options) {
+	options = options || {};
+	var host = options.host || 'graph.facebook.com';
+	var urls = options.urls || ['barackobama', 'facebook', 'google'];
+	var events = options.events || [];
+	
+	return httpAgent.create(host, urls);
+}
+
+var someAgent = createAgent();
+someAgent.start();
+
 vows.describe('httpAgent').addBatch({
   "When using an httpAgent": {
-    topic: function () {
-      return httpAgent.create('foo.com', ['a', 'e', 'c']);
-    },
     "the create() method": {
+			topic: createAgent(),
       "should return a valid httpAgent": function(agent) {
         assert.instanceOf(agent, httpAgent.agent)
         assert.equal(agent.nextUrls.length, 3);
-        assert.equal(agent.nextUrls[0], 'foo.com/a');
+        assert.equal(agent.nextUrls[0], 'graph.facebook.com/barackobama');
         assert.equal(agent.prevUrls.length, 0);
-        assert.equal(agent.host, 'foo.com');
+        assert.equal(agent.host, 'graph.facebook.com');
       },
       "should return a valid event emitter": function(agent) {
         assert.isFunction(agent.addListener);
@@ -32,19 +42,23 @@ vows.describe('httpAgent').addBatch({
       }
     },
     "the start() method": {
-      topic: function(agent) {
-        agent.addListener('start', this.callback);
-        agent.start();
-      },
+      topic: function () {
+				var agent = createAgent();
+				agent.addListener('start', this.callback);
+				agent.start();
+			},
       "should emit the started event": function(e, agent) {
         assert.instanceOf(agent, httpAgent.agent);
+				assert.equal(agent.nextUrls.length, 2);
+				assert.equal(agent.nextUrls[0], 'graph.facebook.com/facebook')
       }
     },
     "the stop() method": {
-      topic: function(agent) {
-        agent.addListener('stop', this.callback);
-        agent.stop();
-      },
+      topic: function () {
+				var agent = createAgent();
+				agent.addListener('stop', this.callback);
+				agent.stop();
+			},
       "should emit the stopped event": function(e, agent) {
         assert.instanceOf(agent, httpAgent.agent);
       }

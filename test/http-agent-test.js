2b4f83ece5f5a75c1ed56cf6ce83543d13269680
indexzero <charlie.robbins@gmail.com>
2010-07-08 21:44:07 -0400

    [api] Made http-agent raise the next event on each successive request

--- test/http-agent-test.js
+++ test/http-agent-test.js
@@ -20,47 +20,78 @@ function createAgent (options) {
   return httpAgent.create(host, urls);
 }
 
+function createServer (options) {
+  options = options || {};
+  var port = options.port || 8080;
+  
+  http.createServer(function (req, res) {
+    res.sendHeader(200, {'Content-Type': 'text/plain'});
+    eyes.inspect(req);
+    res.end();
+  }).listen(port);
+}
 
 vows.describe('httpAgent').addBatch({
   "When using an httpAgent": {
-    "the create() method": {
-      topic: createAgent(),
-      "should return a valid httpAgent": function(agent) {
-        assert.instanceOf(agent, httpAgent.agent)
-        assert.equal(agent.nextUrls.length, 3);
-        assert.equal(agent.nextUrls[0], 'graph.facebook.com/barackobama');
-        assert.equal(agent.prevUrls.length, 0);
-        assert.equal(agent.host, 'graph.facebook.com');
-      },
-      "should return a valid event emitter": function(agent) {
-        assert.isFunction(agent.addListener);
-        assert.isFunction(agent.removeListener);
-        assert.isFunction(agent.removeAllListener);
-        assert.isFunction(agent.listeners);
-        assert.isFunction(agent.emit);
-      }
-    },
-    "the start() method": {
-      topic: function () {
-        var agent = createAgent();
-        agent.addListener('start', this.callback);
-        agent.start();
-      },
-      "should emit the started event": function(e, agent) {
-        assert.instanceOf(agent, httpAgent.agent);
-        assert.equal(agent.nextUrls.length, 2);
-        assert.equal(agent.nextUrls[0], 'graph.facebook.com/facebook')
-      }
-    },
-    "the stop() method": {
-      topic: function () {
-        var agent = createAgent();
-        agent.addListener('stop', this.callback);
-        agent.stop();
-      },
-      "should emit the stopped event": function(e, agent) {
-        assert.instanceOf(agent, httpAgent.agent);
+    "to browse a path of urls": {
+      "the next event": {
+        topic: function() {
+          var agent = createAgent();
+          agent.addListener('next', this.callback);
+          agent.start();
+          return agent;
+        },
+        "should be raised after start": function(e, agent) { 
+          assert.instanceOf(agent, httpAgent.agent);
+          agent.next();
+        }
       }
     }
   }
-}).export(module);
+})/*.addBatch({
+  "When using an httpAgent": {
+    "simple usage of": {
+      "the create() method": {
+        topic: createAgent(),
+        "should return a valid httpAgent": function(agent) {
+          assert.instanceOf(agent, httpAgent.agent)
+          assert.equal(agent.nextUrls.length, 3);
+          assert.equal(agent.nextUrls[0], 'graph.facebook.com/barackobama');
+          assert.equal(agent.prevUrls.length, 0);
+          assert.equal(agent.host, 'graph.facebook.com');
+        },
+        "should return a valid event emitter": function(agent) {
+          assert.isFunction(agent.addListener);
+          assert.isFunction(agent.removeListener);
+          assert.isFunction(agent.removeAllListener);
+          assert.isFunction(agent.listeners);
+          assert.isFunction(agent.emit);
+        }
+      },
+      "the stop() method": {
+        topic: function () {
+          var agent = createAgent();
+          agent.addListener('stop', this.callback);
+          agent.start();
+          agent.stop();
+        },
+        "should emit the stopped event when previously started": function(e, agent) {
+          assert.instanceOf(agent, httpAgent.agent);
+        }
+      },
+      "the start() method": {
+        topic: function () {
+          var agent = createAgent();
+          agent.addListener('start', this.callback);
+          agent.start();
+        },
+        "should emit the started event": function(e, agent) {
+          assert.instanceOf(agent, httpAgent.agent);
+          assert.equal(agent.nextUrls.length, 2);
+          assert.equal(agent.nextUrls[0], 'graph.facebook.com/facebook');
+        }
+      }
+    },
+  }
+})*/
+.export(module);
